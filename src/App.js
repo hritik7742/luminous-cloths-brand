1a065f2b3446a7f817ff4ddeb7641dd055bfaae9
hritik100 <gd1.calyco@gmail.com>
2024-07-29 23:25:16 +0530

    Update files and add new components

--- src/App.js
+++ src/App.js
@@ -1,63 +1,7 @@
 
 
 // import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Home from './Pages/Home';
-// import ProductList from './Pages/ProductList';
-// import ProductDetail from './Pages/ProductDetail';
-// import Admin from './Admin/Admin';
-// import Loading from './Loading.js'// Make sure the path is correct
-// import './App.css';
-
-// function App() {
-//   const [imagesLoaded, setImagesLoaded] = useState(false);
-
-//   useEffect(() => {
-//     const images = document.querySelectorAll('img');
-//     let loadedCount = 0;
-
-//     const checkIfAllImagesLoaded = () => {
-//       loadedCount++;
-//       if (loadedCount === images.length) {
-//         setImagesLoaded(true);
-//       }
-//     };
-
-//     images.forEach((image) => {
-//       if (image.complete) {
-//         checkIfAllImagesLoaded();
-//       } else {
-//         image.addEventListener('load', checkIfAllImagesLoaded);
-//         image.addEventListener('error', checkIfAllImagesLoaded);
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <Router>
-//       <div className="site-wrapper">
-//         {!imagesLoaded && <Loading />}
-//         <Header />
-//         <main className="site-content" style={{ display: imagesLoaded ? 'block' : 'none' }}>
-//           <Routes>
-//             <Route exact path="/" element={<Home />} />
-//             <Route path="/products" element={<ProductList />} />
-//             <Route path="/product/:id" element={<ProductDetail />} />
-//             <Route path="/admin" element={<Admin />} />
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-// import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 // import Home from './Pages/Home';
@@ -69,6 +13,19 @@
 // import { auth } from './Admin/Firebase'; // Import Firebase auth
 
 // import './App.css';
+
+// function Layout({ children }) {
+//   const location = useLocation();
+//   const showHeaderFooter = location.pathname !== '/login';
+
+//   return (
+//     <>
+//       {showHeaderFooter && <Header />}
+//       <main className="site-content">{children}</main>
+//       {showHeaderFooter && <Footer />}
+//     </>
+//   );
+// }
 
 // function App() {
 //   const [imagesLoaded, setImagesLoaded] = useState(false);
@@ -106,8 +63,7 @@
 //     <Router>
 //       <div className="site-wrapper">
 //         {!imagesLoaded && <Loading />}
-//         <Header />
-//         <main className="site-content" style={{ display: imagesLoaded ? 'block' : 'none' }}>
+//         <Layout>
 //           <Routes>
 //             <Route path="/" element={<Home />} />
 //             <Route path="/products" element={<ProductList />} />
@@ -115,8 +71,7 @@
 //             <Route path="/admin" element={user ? <Admin /> : <Navigate to="/login" />} />
 //             <Route path="/login" element={<Login setUser={setUser} />} />
 //           </Routes>
-//         </main>
-//         <Footer />
+//         </Layout>
 //       </div>
 //     </Router>
 //   );
@@ -124,8 +79,10 @@
 
 // export default App;
 
+
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga'; // Import ReactGA for Google Analytics
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
@@ -138,9 +95,18 @@ import { auth } from './Admin/Firebase'; // Import Firebase auth
 
 import './App.css';
 
+// Initialize Google Analytics
+ReactGA.initialize('G-BRKJDMPL8F'); // Replace with your tracking ID
+
 function Layout({ children }) {
   const location = useLocation();
   const showHeaderFooter = location.pathname !== '/login';
+
+  useEffect(() => {
+    // Track page view
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
 
   return (
     <>
@@ -202,3 +168,4 @@ function App() {
 }
 
 export default App;
+
